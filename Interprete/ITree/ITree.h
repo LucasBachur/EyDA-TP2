d6e3a9d880ec9ee91066910bcd19f50572ffa18a
Lucas Bachur <lucasbachur11@gmail.com>
2020-06-07 02:02:07 -0300

    Agregado comentario para ITree.h

--- Interprete/ITree/ITree.h
+++ Interprete/ITree/ITree.h
@@ -3,8 +3,21 @@
 
 #include "Intervalos/intervalos.h"
 
+/**
+ * Tipo de dato para funciones que se apliquen sobre los datos de los nodos del
+ * arbol.
+ */
 typedef void (*FuncionQueVisita) (Intervalo dato);
 
+
+/**
+ * Estructura que representa cada nodo (o subarbol) de un arbol de intervalos.
+ * Cada nodo tiene:
+ *  - un intervalo
+ *  - un double que representa el maximo extremo derecho de ese subarbol
+ *  - un int que representa la altura de ese subarbol
+ *  - 2 apuntadores a 2 nodos que representan los hijos del subarbol actual
+ */
 typedef struct _ITreeNodo {
   Intervalo intervalo;
   double maxExtDer;
@@ -13,8 +26,17 @@ typedef struct _ITreeNodo {
   struct _ITreeNodo *right;
 } ITreeNodo;
 
+/**
+ * Estructura de ITree.
+ * El ITree (o arbol de intervalos) es un tipo de AVLTree que trabaja
+ * especificamente con intervalos.
+ */
 typedef ITreeNodo *ITree;
 
+/**
+ * Definimos estas 2 macros que toman 2 y 3 numeros, respectivamente,
+ * y devuelven el maximo de entre esos numeros.
+ */
 #define max2(x, y) (((x) > (y)) ? (x) : (y))
 #define max3(x, y, z) (max2 (x, max2 (y, z)))
 
@@ -63,38 +85,81 @@ ITree itree_rotacion_der (ITree arbol);
  */
 ITree itree_rotacion_izq (ITree arbol);
 
-// FALTA COMENTAR
-
 
 /**
- *  Dado un arbol y un objeto, lo inserta.
+ *  Toma un arbol y un intervalo.
+ *  Inserta el intervalo en el arbol respetando las propiedades del arbol AVL.
+ *  Luego chequea si cada subarbol que se cambio cuando se ingreso el nodo
+ *  quedo desbalanceado. En ese caso lo balancea aplicando las rotaciones
+ *  correspondientes.
  */
 void itree_insertar (ITree *arbol, Intervalo);
 
-// No se sabe.
+/**
+ *  Toma un arbol y un intervalo.
+ *  Elimina el intervalo del arbol respetando las propiedades del arbol AVL.
+ *  Luego chequea si cada subarbol que se cambio cuando se elimino el nodo
+ *  quedo desbalanceado. En ese caso lo balancea aplicando las rotaciones
+ *  correspondientes.
+ */
 void itree_eliminar_dato (ITree *arbol, Intervalo);
-// No se sabe.
+
+/**
+ *  Toma un apuntador a un arbol.
+ *  Elimina el nodo que se encuentra mas a la izquierda de ese arbol y
+ *  devuelve el intervalo que se encontraba en el nodo eliminado.
+ */
 Intervalo itree_eliminar_minimo (ITree *arbol);
 
 
 /**
- * Recorre en profundidad y aplica la funcion visitante.
+ * Toma un arbol una funcion visitante.
+ * Recorre el arbol en profundidad y aplica la funcion visitante a cada nodo
+ * del arbol.
  */
 void itree_recorrer_dfs (ITree arbol, FuncionQueVisita visit);
 
 /**
- * Recorre en anchura y aplica la funcion visitante.
+ * Toma un arbol una funcion visitante.
+ * Recorre el arbol en su ancho y aplica la funcion visitante a cada nodo
+ * del arbol.
  */
 void itree_recorrer_bfs (ITree arbol, FuncionQueVisita visit);
 
+/**
+ * Toma un apuntador a un arbol.
+ * Calcula el maximo extremo derecho de intervalo para ese subarbol
+ * y lo guarda en el parametro maxExtDer del nodo raiz del subarbol que se
+ * tomo como parametro.
+ */ 
 void itree_mayor_extDer (ITree *arbol);
 
+/**
+ * Toma un apuntador a un arbol.
+ * Calcula la altura de ese subarbol y lo guarda en el parametro
+ * altura del nodo raiz del subarbol que se tomo como parametro.
+ */
 void itree_actualizar_altura (ITree *arbol);
 
+/**
+ * Toma un arbol y libera todos sus nodos.
+ */
 void itree_destruir (ITree raiz);
 
+/**
+ * Toma un arbol y un intervalo.
+ * Busca si dentro del arbol hay por lo menos un intervalo que interseque con
+ * el intervalo parametro. En caso de ser asi devuelve un subarbol cuyo primer
+ * nodo interseca con el intervalo parametro. En caso contrario devuelve NULL.
+ */
 ITree itree_intersecar (ITree, Intervalo);
 
+/**
+ * Definimos estas 2 funciones para imprimir el arbol de una forma legible
+ * dentro de la consola de comandos.
+ * COUNT es la constante que marca la cantidad de espacios que hay entre cada
+ * nodo y sus hijos.
+ */
 #define COUNT 10
 
 void print2D(ITree arbol);
